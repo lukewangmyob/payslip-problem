a6b5c82cb8f03345287033cbdcbc02f6ead538b5
Luke Wang <luke.wang@myob.com>
2021-08-15 16:31:26 +1000

    initial setup

---
+++ src/PaySlipProblem/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PaySlipProblem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
