9456564b601e39025ae95479d57f1bd1521457db
Luke Wang <luke.wang@myob.com>
2021-08-15 20:53:02 +1000

    added logic to catch user exit

--- src/PaySlipProblem/Program.cs
+++ src/PaySlipProblem/Program.cs
@@ -9,13 +9,21 @@ namespace PaySlipProblem
         {
             var consoleUtils = new ConsoleUtils();
             var consoleReader = new ConsoleReader(consoleUtils);
-            var employeeDetailsReader = new EmployeeDetailsReader(consoleReader);
-            var employeeDetails = employeeDetailsReader.Read();
-            var payslip = PaySlipGenerator.Generate(employeeDetails);
             
-            consoleUtils.Write("\nYour payslip has been generated:\n");
-            consoleUtils.Write(payslip.Output());
-            consoleUtils.Write("\nThank you for using MYOB!");
+            try
+            {
+                var employeeDetailsReader = new EmployeeDetailsReader(consoleReader);
+                var employeeDetails = employeeDetailsReader.Read();
+                var payslip = PaySlipGenerator.Generate(employeeDetails);
+            
+                consoleUtils.Write("\nYour payslip has been generated:\n");
+                consoleUtils.Write(payslip.Output());
+                consoleUtils.Write("\nThank you for using MYOB!");
+            }
+            catch (QuitApplicationException exception)
+            {
+                consoleUtils.Write("Application has exited successfully");
+            }
         }
     }
 }
